99036e714d342d3c38f9147c2f2daea732b505ff
Dan Valentine <danvalentine256@gmail.com>
2023-04-11 11:03:06 -0400

    tweak colors

--- init.lua
+++ init.lua
@@ -3,4 +3,8 @@ require("config.autocmds")
 require("config.options")
 require("plugins.setup")
 
+vim.g.sonokai_style = 'atlantis'
+vim.g.sonokai_better_performance = 1
+vim.g.sonokai_enable_italic = 1
+
 vim.cmd('colorscheme sonokai')
